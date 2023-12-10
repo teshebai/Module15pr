a9bac8b6425dd576d4eb6f620ee6557cdf370a53
Teshebayev <eraliteshebai2002gmail.com>
2023-12-10 20:43:21 +0600

    15

---
+++ Module15pr/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module15pr
+{
+    public class MyClass
+    {
+
+
+        private int privateField = 42;
+        public string publicField = "Public Field";
+
+        public int PublicProperty { get; set; }
+        private string PrivateProperty { get; set; }
+
+
+        public MyClass()
+        {
+        }
+
+
+
+        private MyClass(int value)
+        {
+            this.PrivateProperty = "Private Property";
+            this.PublicProperty = value;
+        }
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public Method");
+        }
+
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private Method");
+        }
+
+
+
+
+        public int CalculateSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Module15pr/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module15pr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
